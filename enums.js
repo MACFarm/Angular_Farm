32b0b14d11463afe463e10e0050f774d613f590a
MACFarm <148879969+MACFarm@users.noreply.github.com>
2023-10-24 15:52:35 +0200

    Add files via upload

---
+++ enums.js
@@ -0,0 +1,37 @@
+/* 
+### **Fundamentos de JavaScript**
+
+1. **Sintaxis y Operadores**: Cómo se escriben las instrucciones, cómo se usan los operadores aritméticos, lógicos, etc.
+2. **Tipos de Datos**: Números, cadenas de texto, booleanos, **`null`**, **`undefined`**, y otros.
+3. **Estructuras de Control**: Bucles como **`for`**, **`while`**, y sentencias condicionales como **`if-else`**.
+4. **Funciones**: Cómo declararlas, parámetros, ámbito (scope) y closures.
+
+### **Programación Orientada a Objetos (OOP)**
+
+1. **Objetos y Clases**: Cómo crearlos y cómo funcionan.
+2. **Herencia y Polimorfismo**: Extender clases y usar múltiples formas del mismo método o función.
+
+### **DOM Manipulation**
+
+1. **Selección de Elementos**: Usar métodos como **`getElementById`**, **`querySelector`**, etc.
+2. **Modificación del DOM**: Añadir, eliminar o modificar elementos y atributos.
+3. **Event Handling**: Cómo manejar eventos como clicks, entrada de teclado, etc.
+
+### **Asincronía**
+
+1. **Callbacks**: El concepto básico detrás de la asincronía en JavaScript.
+2. **Promesas**: Un enfoque más moderno para manejar la asincronía.
+3. **`async/await`**: La forma más nueva y legible de manejar operaciones asíncronas.
+
+### **Herramientas y Librerías**
+
+1. **NPM**: El manejador de paquetes de Node.js que te permite instalar bibliotecas.
+2. **Frameworks**: Angular, React, Vue, etc., para facilitar el desarrollo.
+3. **Herramientas de Build**: Webpack, Babel, etc., para preparar tu código para producción.
+
+### **Otras Cosas Chéveres**
+
+1. **ES6+ Características**: Cosas como **`let/const`**, arrow functions, destructuring, etc.
+2. **APIs Web**: Fetch, Local Storage, etc.
+3. **Testing**: Conocimientos básicos en pruebas con Jest o Mocha.
+*/
